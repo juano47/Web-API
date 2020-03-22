6cd4ae64edd471e208254b6ada0583c0af1b7c07
juano47 <juan.delaiglesia.jdli@gmail.com>
2020-03-22 20:30:53 -0300

    Se agregó modelo y controlador de muestra. Además se instalo una extensión cool para test de api

---
+++ WebApi/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models {
+    public class Person {
+        public Int32 ID { get; set; }
+        public String name { get; set; }
+    }
+}
